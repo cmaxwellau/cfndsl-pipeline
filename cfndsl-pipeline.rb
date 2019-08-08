ee51576e2d8ed9fdb27f0b97bdebab6b5b8fbee0
Cam Maxwell <camm@amazon.com>
2019-08-08 22:30:19 +1000

    remove unused file

--- cfndsl-pipeline.rb
+++
@@ -1,9 +0,0 @@
-
-# require 'lib/cfndsl-pipeline'
-# require 'lib/cp-cfndsl'
-# require 'lib/cp-linter'
-# require 'lib/cp-options'
-# require 'lib/cp-params'
-# require 'lib/cp-syntax'
-# require 'lib/monkey-patches'
-# require 'lib/stdout-capture'
