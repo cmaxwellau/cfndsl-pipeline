c8d11e2dcf2cb0046455cab4456a6f286ab07f52
Cam Maxwell <camm@amazon.com>
2019-09-12 15:15:59 +1000

    Revert filename change to main file. thanks rubocop.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,10 @@
-require "bundler/setup"
-require "cfndsl"
-require "cfndsl_pipeline"
+require 'bundler/setup'
+require 'cfndsl'
+require 'cfndsl-pipeline'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
