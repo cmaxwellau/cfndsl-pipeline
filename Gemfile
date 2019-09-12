cb7130bf93cd36fbbf00ff08f9a302fbeba9c1ca
Cam Maxwell <camm@amazon.com>
2019-09-12 12:08:11 +1000

    Updated gemfile

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source "https://rubygems.org"
 repo_name = "cmaxwellau/cfndsl-pipeline"
+gem 'rubocop', '~> 0.29.0', require: false
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
--- lib/logging.rb
+++
@@ -1,9 +0,0 @@
-require 'logger'
-
-module CfnDslPipeline
-	formatter = proc do |severity, datetime, progname, msg|
-	   "#{severity}: #{msg}\n"
-	end
-
-	Logger = Logger.new(STDOUT, formatter: formatter)
-end
